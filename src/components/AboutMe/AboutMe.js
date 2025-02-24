b710610588db6cdc2824448e8eecf16dc32847e8
Edsson <e_d_sx2@hotmail.com>
2025-02-24 16:57:02 -0400

    fixed app css, swapped texts to english

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -64,8 +64,8 @@ function AboutMeSection (){
         <section className="AboutMeSection" id="aboutMe">
             <div className="aboutMeContainer">
                 <div className="descripcion">
-                    <h1>ACERCA DE MI</h1>
-                    <p>Me apasiona la ciencia y la tecnología, siempre me emociona descubrir como funcionan las cosas así que disfruto de aprender algo nuevo cada día. <br/>¿Qué te puedo decir? Ando dando mis primeros pasos en este mundo digital y me intriga saber toda la caminata que me espera.<br/>Hey! pero no te creas que soy un aburrido que solo habla de estudios y trabajo, si me invitas a ver una peli al cine, a nadar a la piscina o echarle una partida a un videojuego con gusto aceptaré!<br/> Total! La vida es un equilibrio entre los deberes y placeres. </p>
+                    <h1>ABOUT ME</h1>
+                    <p>I love science and technology, I am always excited to discover how things work so I enjoy learning something new every day. <br/> What can I tell you? I'm taking my first steps in this digital world and I'm intrigued by the journey that awaits me. <br/>Hey! but don't think that I'm a bore who only talks about studies and work, if you invite me to watch a movie at the cinema, swim in the pool or play a video game I will gladly accept!<br/>Life is a balance between duties and pleasures.</p>
                 </div>
                 <div className="photoSection" >
                     <div className="yoContainer">
@@ -74,7 +74,7 @@ function AboutMeSection (){
                 </div>
             </div>
             <div className="skillsSection">
-                <h1>HABILIDADES</h1>
+                <h1>SKILLS</h1>
                         <div className="skillsReal">
                         {Skills.map((Skill,index)=>(
                             <SkillsCards name={Skill.Name} image={Skill.Logo} nivel={Skill.Nivel} key={index}/>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,9 +10,9 @@ function HeroSection(){
     return(
         <section className="hero">
             <div className="titulo">
-                <h2 className="position">¡Hola! Me llamo</h2>
+                <h2 className="position">Hello! My name is</h2>
                 <h1 className="name">EDSSON VEGA</h1>
-                <h2 className="position">Desarrollador Front End</h2>
+                <h2 className="position">Front End developer</h2>
                 <a  href={cv} download>
                     <button className="resumeButton">
                         <GrDocumentDownload className="hoja"/>RESUME
